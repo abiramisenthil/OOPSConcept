5e3b970736770e219e869b9b2b25cc4633e94e1d
abiramisenthil <abis.senthil@gmail.com>
2019-11-14 11:00:12 +0000

    Add files via upload

---
+++ Oops/AbstractAndEncapsulation/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace AbstractAndEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer.Customer obj = new Customer.Customer();
+            obj.CustomerCode = "CustomerCode";
+            obj.CustomerName = "CustomerName";
+            obj.Age = 35;
+            
+            
+            /* The end user should not be exposed to Validate and CreateDBObjects functions.
+             * We are complicating his life.
+             * So, we can make these two functions are private to make these functions not exposed to end user
+             *
+             */
+            // obj.Validate();
+            // obj.CreateDBObjects();
+            obj.AddCustomerDataIntoDb();
+        }
+    }
+}
---
+++ Oops/Customer/Customer.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Customer
+{
+    public class Customer
+    {
+        
+        public string CustomerCode { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public int Age { get; set; }
+
+        public void AddCustomerDataIntoDb()
+        {
+            Validate();
+            CreateDBObjects();
+            // Code implementation to add data into database
+        }
+
+        // Granular functions **** Made the these complicated methods as private (Encapsulation)
+
+        private bool Validate()
+        {
+            // Validate customer code, name and age.
+            return true;
+        }
+
+        private bool CreateDBObjects()
+        {
+            // DB connections, SQL etc
+            return true;
+
+        }
+
+    }
+}
